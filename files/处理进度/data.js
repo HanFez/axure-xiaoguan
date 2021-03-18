e208f5fae98ab3017f3cda5ac28d8e0867f7d89e
HanFei <feezz@qq.com>
2021-03-18 10:11:14 +0800

    上传原型图项目文件

---
+++ files/处理进度/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,_(g,h,i,_(j,k,l,k)),m,[],n,_(h,o),p,[q,r],s,_(t,u,v,w,g,x,y,_(),z,[],A,_(B,C,D,E,F,_(G,H,I,J),K,null,L,M,M,N,O,P,null,Q,R,S,T,U,V,W,R,X,Y,_(G,H,I,Z),ba,R,bb,bc,_(bd,be,bf,bg,bh,bg,bi,bg,bj,k,I,_(bk,bl,bm,bl,bn,bl,bo,bp)),i,_(j,k,l,k)),bq,_(),br,_(),bs,_(bt,[])),bu,_(),bv,_());}; 
+var b="url",c="处理进度.html",d="generationDate",e=new Date(1616033167331.13),f="defaultAdaptiveView",g="name",h="",i="size",j="width",k=0,l="height",m="adaptiveViews",n="sketchKeys",o="s0",p="variables",q="optime",r="currentTime",s="page",t="packageId",u="a0829af5af244a8e925c4c09f7d53a23",v="type",w="Axure:Page",x="处理进度",y="notes",z="annotations",A="style",B="baseStyle",C="627587b6038d43cca051c114ac41ad32",D="pageAlignment",E="center",F="fill",G="fillType",H="solid",I="color",J=0xFFFFFFFF,K="image",L="imageAlignment",M="near",N="imageRepeat",O="auto",P="favicon",Q="sketchFactor",R="0",S="colorStyle",T="appliedColor",U="fontName",V="Applied Font",W="borderWidth",X="borderVisibility",Y="borderFill",Z=0xFF797979,ba="cornerRadius",bb="cornerVisibility",bc="outerShadow",bd="on",be=false,bf="offsetX",bg=5,bh="offsetY",bi="blurRadius",bj="spread",bk="r",bl=0,bm="g",bn="b",bo="a",bp=0.349019607843137,bq="adaptiveStyles",br="interactionMap",bs="diagram",bt="objects",bu="masters",bv="objectPaths";
+return _creator();
+})());
